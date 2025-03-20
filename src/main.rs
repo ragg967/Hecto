0e134b633ef609aa8de3498dfc48b423b137eab6
Ragg967 <rywatson1027@gmail.com>
2025-03-19 19:10:16 -0700

    first commit!!

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::io::{self, Read};
+
+fn main() {
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+
+        if c = 'q' {
+            break;
+        }
+    }
+}
